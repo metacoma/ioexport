12ee7b8f92e8d1281bb85af828ad8ffdf6964f8f
Sergey (scor) Bubnov <omgbebebe@gmail.com>
2013-08-16 09:21:44 +0400

    add make_rw && make_ro functions

--- mod/ioexp.c
+++ mod/ioexp.c
@@ -22,52 +22,71 @@ struct pid *pid_struct;
 struct task_struct *task;
 
 
-unsigned long *syscall_table = (unsigned long *)SCT;
+/* Make the page writable */
+int make_rw(unsigned long address)
+{
+    unsigned int level;
+    pte_t *pte = lookup_address(address, &level);
+    if(pte->pte &~ _PAGE_RW)
+        pte->pte |= _PAGE_RW;
+    return 0;
+}
+
+/* Make the page write protected */
+int make_ro(unsigned long address)
+{
+    unsigned int level;
+    pte_t *pte = lookup_address(address, &level);
+    pte->pte = pte->pte &~ _PAGE_RW;
+    return 0;
+}
+
+unsigned long *sys_call_table = (unsigned long *)SCT;
 
 asmlinkage int (*ref_read)(unsigned int, char __user *, size_t);
 
 asmlinkage int new_read(unsigned int fd, char __user *buf, size_t count)
 {
-	//printk(KERN_ALERT "WRITE HIJACKED");
-	int ret;
-	in_use[in_use_ndx] = current->pid;
-	in_use_ndx++;
+    //printk(KERN_ALERT "WRITE HIJACKED");
+    int ret;
+    in_use[in_use_ndx] = current->pid;
+    in_use_ndx++;
     ret = ref_read(fd, buf, count);
-	in_use_ndx--;
-	return ret;
+    in_use_ndx--;
+    return ret;
 }
 
 static int __init start(void)
 {
-	memset(in_use, 0, MAX_PIDS);
+    memset(in_use, 0, MAX_PIDS);
 
     printk(KERN_ALERT "\nHIJACK INIT\n");
 
-	write_cr0 (read_cr0 () & (~ 0x10000));
-		ref_read = (void *)syscall_table[__NR_read];
-	    syscall_table[__NR_read] = new_read;
-	write_cr0 (read_cr0 () | 0x10000);
+    make_rw((unsigned long)sys_call_table);
+    ref_read = (void *)sys_call_table[__NR_read];
+    sys_call_table[__NR_read] = new_read;
+    make_ro((unsigned long)sys_call_table);
 
     return 0;
 }
 
 static void __exit end(void)
 {
-	unsigned int i = 0;
-	pid_t p_id = 0;
-	for (i = 0; i <= in_use_ndx; i++){
-		p_id = in_use[i];
-		pid_struct = find_get_pid(p_id);
-		task = pid_task(pid_struct,PIDTYPE_PID);
-		printk("%u: pid %u\tcomm: %s\n", i, in_use[i], task->comm);
-	}
+    unsigned int i = 0;
+    pid_t p_id = 0;
+    for (i = 0; i <= in_use_ndx; i++){
+        p_id = in_use[i];
+        pid_struct = find_get_pid(p_id);
+        task = pid_task(pid_struct,PIDTYPE_PID);
+        printk("%u: pid %u\tcomm: %s\n", i, in_use[i], task->comm);
+    }
 
-	write_cr0 (read_cr0 () & (~ 0x10000));
-		syscall_table[__NR_read] = ref_read;  
-	write_cr0 (read_cr0 () | 0x10000);
+    make_rw((unsigned long)sys_call_table);
+    sys_call_table[__NR_read] = ref_read;  
+    make_ro((unsigned long)sys_call_table);
 
     printk(KERN_ALERT "MODULE EXIT\n");
-	return;
+    return;
 }
 
 module_init(start);
